d8803bb90c30fd151603d46dc9840082092a31b3
DELL@Alsaeed <alsaeedalsafer@gmail.com>
2025-01-22 02:53:43 +0200

    Print text

---
+++ ConsoleApplication2/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "***************************\n";
+    cout << "Name : Alsaeed Abubaker \n";
+    cout << "Age  : 30 Years\n";
+    cout << "City : Khartom\n";
+    cout << "Cantry : Sudan\n";
+    cout << "***************************\n";
+
+    return 0;
+}
+
+
